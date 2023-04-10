7c61a4063f836eeb6e14305eab70f46993c53329
dr2xmillion371 <119767559+dr2xmillion371@users.noreply.github.com>
2023-04-10 10:10:57 -0500

    Add files via upload

---
+++ one.py
@@ -0,0 +1,15 @@
+import time as tea
+
+
+
+
+def TowerOfHanoiMoves(s,e):
+    for i in range(s,e):
+      print('No. of moves: %s' % i)
+      tea.sleep(0.012545)
+      
+      
+      
+
+
+TowerOfHanoiMoves(0,32769)#32768 because 2^15 is 32768
